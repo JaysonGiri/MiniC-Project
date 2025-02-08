c44cd81ec90a6bc95267751d8041965b79733f01
JaysonGiri <sanjaygiriprakash@unomaha.edu>
2025-02-08 10:40:27 -0600

    Added Input check and random number generator

--- NumGuess/Program.cs
+++ NumGuess/Program.cs
@@ -33,7 +33,13 @@ namespace NumberGuesser
             Console.WriteLine("Hello {0}, let's play a game...", inputName);
 
             // Init correct number
-            int correctNumber = 7;
+            // int correctNumber = 7;
+
+            // Create a new Random object
+            Random random = new Random();
+
+            // Init correct number
+            int correctNumber = random.Next(1, 10);
 
             // Init guess var
             int guess = 0;
@@ -46,6 +52,22 @@ namespace NumberGuesser
             {
                 // Get user input
                 string input = Console.ReadLine();
+
+                // Make sure its a number
+                if (!int.TryParse(input, out guess))
+                {
+                    // Change text color
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    // Tell user its not a number
+                    Console.WriteLine("Please enter an actual number...");
+
+                    // Reset text color
+                    Console.ResetColor();
+
+                    // Keep going
+                    continue;
+                }
 
                 // Cast to int and put in guess
                 guess = Int32.Parse(input);
@@ -61,7 +83,6 @@ namespace NumberGuesser
 
                     // Reset text color
                     Console.ResetColor();
-
 
                 }
 
