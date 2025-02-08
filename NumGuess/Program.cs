0a5e9e5187e3366d33cba553ef7e8eb46d986e2d
JaysonGiri <sanjaygiriprakash@unomaha.edu>
2025-02-08 09:58:51 -0600

    First Commit, Number Guesser Project setup

---
+++ NumGuess/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Namspace
+namespace NumberGuesser
+{
+    // Main Class
+    internal class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+            // START HERE //
+            Console.Write("Hello World");
+        }
+    }
+}
